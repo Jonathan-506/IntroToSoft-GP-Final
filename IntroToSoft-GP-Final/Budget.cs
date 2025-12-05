03e388406a76d40cc93e2e6569a56b05e457c3d7
AtleySetup <jonathan.owen@students.snow.edu>
2025-12-05 16:38:56 -0700

    Added Budget Work that Jonathan did

---
+++ IntroToSoft-GP-Final/Budget.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToSoft_GP_Final
+{
+    public class Budget
+    {
+        internal decimal BudgetAmount { get; set; }
+
+        //internal List<Categories> BudgetCategories { get; set; }
+
+
+        public Budget()
+        {
+        }
+
+
+
+
+        internal void Withraw(Transaction transaction)
+        {
+            if (transaction.Amount < 0)
+            {
+                transaction.Amount = 0;
+            }
+            OverDraft();
+            BudgetAmount -= transaction.Amount;
+        }
+
+
+
+        internal void OverDraft()
+        {
+            if (BudgetAmount < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ALERT, YOU HAVE OVERDRAFTED YOUR BUDGET PLEASE READJUST YOUR BUDGET TO COMPENSATE");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press any key to accept this message");
+                Console.ReadKey(true);
+
+            }
+        }
+
+        internal void Add(Transaction transaction)
+        {
+            if (transaction.Amount < 0)
+            {
+                transaction.Amount = 0;
+            }
+
+            BudgetAmount += transaction.Amount;
+        }
+
+        internal void EditBudget(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Invalid Budget Amount");
+            }
+
+            BudgetAmount = amount;
+        }
+    }
+}
---
+++ IntroToSoft-GP-Final/BudgetTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+
+namespace IntroToSoft_GP_Final
+{
+
+
+    public class BudgetTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+
+        public void TestWithdrawMethod()
+        {
+            Budget budget = new Budget();
+            budget.BudgetAmount = 150;
+
+            Transaction transaction = new Transaction(75, "bills", "Electric Co.");
+
+            budget.Withraw(transaction);
+
+            Assert.That(budget.BudgetAmount, Is.EqualTo(75));
+        }
+
+        public void TestWithdrawMethodWithNegativeNumber()
+        {
+            Budget budget = new Budget();
+            budget.BudgetAmount = 150;
+
+            Transaction transaction = new Transaction(-125, "bills", "Electric Co.");
+
+            budget.Withraw(transaction);
+
+            Assert.That(budget.BudgetAmount, Is.EqualTo(150));
+        }
+
+        public void TestAdditionToBudget()
+        {
+            Budget budget = new Budget();
+            budget.BudgetAmount = 150;
+
+            Transaction transaction = new Transaction(32, "Pay u back", "Mikey");
+
+            budget.Add(transaction);
+
+            Assert.That(budget.BudgetAmount, Is.EqualTo(182));
+        }
+
+        public void TestAdditionWithNegativeNumber()
+        {
+            Budget budget = new Budget();
+            budget.BudgetAmount = 516;
+
+            Transaction transaction = new Transaction(-87, "Pay u back", "Mikey");
+
+            budget.Add(transaction);
+
+            Assert.That(budget.BudgetAmount, Is.EqualTo(516));
+        }
+
+
+        public void TestChangeBudgetAmount()
+        {
+            Budget budget = new Budget();
+            budget.BudgetAmount = 1000;
+
+            decimal amount = 1600;
+
+            budget.EditBudget(amount);
+
+            Assert.That(budget.BudgetAmount, Is.EqualTo(amount));
+
+        }
+    }
+}
